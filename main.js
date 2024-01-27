868c05bbf33fc18d1bfb1dc2cab41f860ce53f3c
Kalmesh-ms <141908865+Kalmesh-ms@users.noreply.github.com>
2024-01-27 19:30:54 +0530

    Initial dist subtree commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("YOOO")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG1CIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIllPT09cIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
